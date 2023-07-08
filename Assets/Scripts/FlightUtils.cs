0ef2fce0f1ba16f2089a4e9a32ea4b8251c2c8d2
njordsoevik <njordsoevik@gmail.com>
2023-07-08 13:38:24 -0700

    updated flight

--- Assets/Scripts/FlightUtils.cs
+++ Assets/Scripts/FlightUtils.cs
@@ -10,7 +10,6 @@ public class FlightUtils : MonoBehaviour
     public InputActionReference _rightHandPosition;
     public InputActionReference _leftVelocityReference;
     public InputActionReference _rightVelocityReference;
-
     public float maxWingSpan = 1.7f; // TODO: use GetHandsDistanceHorizontal in a config scene
 
     public Vector3 LeftHandVelocity()
@@ -56,31 +55,63 @@ public class FlightUtils : MonoBehaviour
         return Vector3.Distance(lPosition, rPosition);
     }
 
+    public float HandHeightLeft()
+    {
+        Debug.Log("-----------START---------");
+        Debug.Log(_leftHandPosition.action.ReadValue<Vector3>());
+        Debug.Log(_leftHandPosition.action.ReadValue<Vector3>().y);
+        return _leftHandPosition.action.ReadValue<Vector3>().y;
+    }
+
+    public float HandHeightRight()
+    {
+        return _leftHandPosition.action.ReadValue<Vector3>().y;
+    }
+
     public float AngleDownRight()
     {
 
-        Vector3 headToRight = Vector3.Normalize(HeadToRightDistance());
+        Vector3 headToRight = Vector3.Normalize(HeadToRightVector());
         return Vector3.Angle(headToRight, Vector3.down);
     }
 
     public float AngleDownLeft()
     {
 
-        Vector3 headToLeft = Vector3.Normalize(HeadToLeftDistance());
+        Vector3 headToLeft = Vector3.Normalize(HeadToLeftVector());
         return Vector3.Angle(headToLeft, Vector3.down);
     }
 
-    private Vector3 HeadToRightDistance()
+    private Vector3 HeadToRightVector()
     {
         Vector3 rPosition = _rightHandPosition.action.ReadValue<Vector3>();
         Vector3 headPosition = _headPosition.action.ReadValue<Vector3>();
+
         return rPosition - headPosition;
     }
 
-    private Vector3 HeadToLeftDistance()
+    private Vector3 HeadToLeftVector()
     {
         Vector3 lPosition = _leftHandPosition.action.ReadValue<Vector3>();
         Vector3 headPosition = _headPosition.action.ReadValue<Vector3>();
         return lPosition - headPosition;
     }
+    public float HeadToLeftDistance()
+    {
+        Vector3 lPosition = _leftHandPosition.action.ReadValue<Vector3>();
+        Vector3 headPosition = _headPosition.action.ReadValue<Vector3>();
+        lPosition.y = 0.0f;
+        headPosition.y = 0.0f;
+        return Vector3.Distance(lPosition, headPosition);
+    }
+
+    public float HeadToRightDistance()
+    {
+        Vector3 rPosition = _rightHandPosition.action.ReadValue<Vector3>();
+        Vector3 headPosition = _headPosition.action.ReadValue<Vector3>();
+        rPosition.y = 0.0f;
+        headPosition.y = 0.0f;
+        return Vector3.Distance(rPosition, headPosition);
+    }
+
 }
--- Assets/Scripts/Flightv2.cs
+++ Assets/Scripts/Flightv2.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(FlightUtils))]
+[RequireComponent(typeof(Rigidbody))]
 public class Flightv2 : MonoBehaviour
 {
     public Transform leftPad;
@@ -9,12 +12,15 @@ public class Flightv2 : MonoBehaviour
 
     public InputActionReference _leftFlightButton;
     public InputActionReference _rightFlightButton;
-    public float flapForce = 0.5f;
+    public float flapForce = 0.4f;
+    public float liftFactor = 0.7f; // between 0,1, how much negating force your wings have against body velocity
+    public float rotationSpeed = 1f;
     private Rigidbody _rigidBody;
     private bool leftArmActivated;
     private bool rightArmActivated;
     private Vector3 leftHandDownVector;
     private Vector3 rightHandDownVector;
+    private FlightUtils utilsFlight;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,23 +33,79 @@ public class Flightv2 : MonoBehaviour
         leftArmActivated = false;
 
         // Vectors
-        rightHandDownVector = new Vector3(0, 0, 0);
-        leftHandDownVector = new Vector3(0, 0, 0);
+        rightHandDownVector = rightPad.right.normalized;
+        leftHandDownVector = leftPad.right.normalized * -1.0f;
+        utilsFlight = GetComponent<FlightUtils>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        ///////// Palm direction
         rightHandDownVector = rightPad.right.normalized;
         leftHandDownVector = leftPad.right.normalized * -1.0f;
-        if (leftArmActivated)
+
+        ///////// Distance Hands Apart 
+        float percentHandsApartLeft = utilsFlight.HeadToLeftDistance() / (utilsFlight.maxWingSpan / 2.0f);
+
+        float percentHandsApartRight = utilsFlight.HeadToRightDistance() / (utilsFlight.maxWingSpan / 2.0f);
+        percentHandsApartLeft = Mathf.Min(percentHandsApartLeft, 1.0f);
+        percentHandsApartRight = Mathf.Min(percentHandsApartRight, 1.0f);
+
+        if (percentHandsApartLeft < 0.4f) // If below threshold, act as if hands together
         {
-            _rigidBody.AddForce(leftHandDownVector * flapForce, ForceMode.Impulse);
+            percentHandsApartLeft = 0.0f;
         }
-        if (rightArmActivated)
+        if (percentHandsApartRight < 0.4f) // If below threshold, act as if hands together
         {
-            _rigidBody.AddForce(rightHandDownVector * flapForce, ForceMode.Impulse);
+            percentHandsApartRight = 0.0f;
         }
+
+        ///////// Flap force 
+        Vector3 leftVelocity = utilsFlight.LeftHandVelocity() * percentHandsApartLeft;
+        Vector3 rightVelocity = utilsFlight.RightHandVelocity() * percentHandsApartRight;
+        // Less powerful upward motion
+        if (leftVelocity.y > 0)
+        {
+            leftVelocity *= 0.2f;
+        }
+        if (rightVelocity.y > 0)
+        {
+            rightVelocity *= 0.2f;
+        }
+        // Accelerate body with flap force in opposite direction of palm
+        _rigidBody.AddForce((leftHandDownVector * -1.0f) * Vector3.Dot(leftVelocity, leftHandDownVector) * flapForce, ForceMode.Impulse);
+        _rigidBody.AddForce((rightHandDownVector * -1.0f) * Vector3.Dot(rightVelocity, rightHandDownVector) * flapForce, ForceMode.Impulse);
+
+        //////// Lift
+        Vector3 currentVelocity = _rigidBody.velocity;
+        // Equation description
+        // (leftHandDownVector * -1.0f) is the palm direction to apply our lift force
+        // Vector3.Dot(currentVelocity, leftHandDownVector) gets our current velocity in the palms direction, which we want to partially negate
+        // 0.5f because one hand gives half the force
+        // liftFactor so we don't 100% negate our velocity, only partially
+        // percentHandsApart to add control to how much lift we want
+        _rigidBody.AddForce((leftHandDownVector * -1.0f) * Vector3.Dot(currentVelocity, leftHandDownVector) * liftFactor * 0.5f * percentHandsApartLeft, ForceMode.Acceleration);
+        _rigidBody.AddForce((rightHandDownVector * -1.0f) * Vector3.Dot(currentVelocity, rightHandDownVector) * liftFactor * 0.5f * percentHandsApartRight, ForceMode.Acceleration);
+
+
+        // This negates gravity fully
+        // _rigidBody.AddForce(-1f * Physics.gravity, ForceMode.Acceleration);
+
+        //////// Rotation 
+        Debug.Log("utilsFlight.HandHeightLeft()");
+        Debug.Log(utilsFlight.HandHeightLeft());
+        float heightDifference = utilsFlight.HandHeightRight() - utilsFlight.HandHeightLeft();
+        Debug.Log("heightDifference");
+        Debug.Log(heightDifference);
+        float heightPercent = heightDifference / utilsFlight.maxWingSpan; // TODO Not perfect because height difference cant reach max wing span
+        Debug.Log("heightPercent");
+        Debug.Log(heightPercent);                                                              // Rotate the camera horizontally around the y-axis
+
+        transform.Rotate(Vector3.up, heightPercent * -1f * rotationSpeed);
+
+
+
 
     }
     private void LeftFlightActivate(InputAction.CallbackContext context)
